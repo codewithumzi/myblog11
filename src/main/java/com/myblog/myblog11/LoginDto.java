dc519c48653351dba36de1e8e7976d620eebb322
Umar Ashraf Lone <theistummer23@gmail.com>
2024-01-18 12:14:37 +0530

    custom exception handling and get all posts using streamapi

---
+++ src/main/java/com/myblog/myblog11/LoginDto.java
@@ -0,0 +1,23 @@
+package com.myblog.myblog11;
+//CONVERT OBJECTS TO DTOs  USING STREAM API PART 3
+
+public class LoginDto {
+    private String userName;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/myblog/myblog11/Test.java
@@ -0,0 +1,26 @@
+package com.myblog.myblog11;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//coversion of objects to dto
+public class Test {
+    public static void main(String[] args) {
+        List<Login> logins = Arrays.asList(
+//   [com.myblog.myblog11.Login@506c589e, com.myblog.myblog11.Login@69d0a921, com.myblog.myblog11.Login@446cdf90]
+
+                new Login("umar", "testing"),
+                new Login("shakir", "testing"),
+                new Login("akib", "testing")
+        );
+        List<LoginDto> dtos = logins.stream().map(login -> mapToDto(login)).collect(Collectors.toList());
+        System.out.println(dtos);
+    }
+     static  LoginDto mapToDto(Login login){
+        LoginDto dto=new LoginDto();
+        dto.setUserName(login.getUsername());
+        dto.setPassword(login.getPassword());
+        return dto;
+    }
+}
